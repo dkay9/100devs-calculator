ddcc85312e256249e99944fe779d5b0e94f99bdf
David <kaludavid411@gmail.com>
2024-05-09 16:43:45 -0700

    commit

--- template/js/main.js
+++ template/js/main.js
@@ -1,10 +1,11 @@
+//storing input and button value in a variable
 let inputDisplay = document.querySelector('#inputBox')
 let buttons = document.querySelectorAll('button')
-
+//create empty string variable 
 let string = ""
-
+// added event listener to the button and evaluating the input values using forEach method
 buttons.forEach(element => {
-    element.addEventListener('click', (b)=>{
+    element.addEventListener('click', (b) => {
         if(b.target.innerText == '='){
             string = String(eval(string))
             inputDisplay.value = string
